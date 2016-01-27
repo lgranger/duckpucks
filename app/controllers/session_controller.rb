e3c0a03a371893d848d65fe1b9c8bba484ce5566
Riley Spicer <riley.r.spicer@gmail.com>
2016-01-27 10:08:51 -0800

    added session create and destroy code

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -3,8 +3,22 @@ class SessionController < ApplicationController
   end
 
   def create
+    auth_hash = request.env['omniauth.auth']
+    if auth_hash["uid"]
+      @user = User.find_or_create_from_omniauth(auth_hash)
+      if @user
+        session[:user_id] = @user.id
+        redirect_to root_path
+      else
+        redirect_to root_path, notice: "Failed to save the user"
+      end
+    else
+      redirect_to root_path, notice: "Failed to authenticate"
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to root_path
   end
 end
